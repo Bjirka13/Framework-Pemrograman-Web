15fef81e02694588b0019ed1331a1eb9f1e6ed5c
Frans Hutapea <147828133+Bjirka13@users.noreply.github.com>
2025-09-14 00:28:08 +0700

    Delete resources directory

--- resources/views/company_profile.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('app')
-
-@section('content')
-    <h1>Company Profile</h1>
-	<video width="640" height="360" controls>
-    <source src="{{ asset($video) }}" type="video/mp4">
-	</video>
-@endsection
