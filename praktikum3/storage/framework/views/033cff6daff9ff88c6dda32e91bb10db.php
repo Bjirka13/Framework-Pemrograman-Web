0ec4eeea0d24c8c214a7f68371f23591875fe37c
Frans Hutapea <147828133+Bjirka13@users.noreply.github.com>
2025-09-15 11:01:47 +0700

    Delete praktikum3 directory

--- praktikum3/storage/framework/views/033cff6daff9ff88c6dda32e91bb10db.php
+++
@@ -1,5 +0,0 @@
-<ul>
-    <li><a href="<?php echo e(route('home')); ?>">Home</a></li>
-    <li><a href="<?php echo e(route('barang')); ?>">Barang</a></li>
-    <li><a href="<?php echo e(route('about')); ?>">About</a></li>
-</ul><?php /**PATH C:\xampp\htdocs\praktik3-crud\praktik3-crud\resources\views/navbar.blade.php ENDPATH**/ ?>
